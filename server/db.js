160863a04499db3e95b094a6c4db47fb50bb3eec
Nathan Gearke <n.gearke@gmail.com>
2021-05-18 21:41:46 -0500

    Added some routing, tested with postman and started to encrypt password then ran into some internet issues.

--- server/db.js
+++ server/db.js
@@ -8,3 +8,5 @@ const pool = new Pool({
     port: 5432,
     database: "jwttutorial"
 });
+
+module.exports = pool;
--- server/routes/jwtAuth.js
+++ server/routes/jwtAuth.js
@@ -1,24 +1,35 @@
 const router = require("express").Router();
-
+const pool = require("../db");
 // registering
 
-router.post("/", async (req, res) => {
+router.post("/register", async (req, res) => {
     try {
         //1. destructure the req.body (name, email, password)
 
+        const { name, email, password } = req.body; //this assigns the 3 const to req.body.name and so on.
 
         //2. check if user exists (if user exist, throw error)
 
+        const user = await pool.query("SELECT * FROM users WHERE user_email = $1", [email]);
+
+        if (user.rows.length !== 0){
+            //Then we would have a duplicate
+            return res.status(401).send("User already exists."); //401 = Unauthenticated 403 = Unauthorized
+        }
 
         //3. Bcrypt the user password
 
+            const saltRound = 10; //How many times it will encrypt
+            const salt = await bcrypt.genSalt(saltRound);
+
+            const bcryptPassword = bcrypt.hash(password, salt);
 
         //4. enter the new user inside our database
 
-
+            const newUser = await pool.query("INSERT INTO user ")
         //5. generating our jwt token
 
-        
+
 
     } catch (err) {
         console.error(err.message);
